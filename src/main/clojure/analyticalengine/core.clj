10191ba0a79a3d03680d8e63c41628af3bd8c2fc
Ivan willig <ivan@pluto.(none)>
2011-07-09 21:43:05 -0400

    Added what I hope with be a nice interactive development environment for developing with the maven jetty plugin in clojure.

--- src/main/clojure/analyticalengine/core.clj
+++ src/main/clojure/analyticalengine/core.clj
@@ -1,3 +1,33 @@
-(ns analyticalengine.core)
+(ns analyticalengine.core
+  (:use [compojure.core :only (defroutes GET ANY)]
+        [compojure.route :only (not-found)]
+        [ring.middleware.multipart-params :only (wrap-multipart-params)]
+        [ring.adapter.jetty-servlet :only (run-jetty)]))
 
-(def app)
+(defn wrap-servlet-session [handler]
+  (fn [request]
+    (handler
+     (if-let [servlet-request (:servlet-request request)]
+       (assoc request :session (.getSession servlet-request true))
+       request))))
+
+(defn wrap-logging [handler]
+  (fn [request]
+    (println (keys request))
+    (handler request)))
+
+(defn index [request]
+  (str request))
+
+
+(defroutes main-routes
+  (GET "/" [] index)
+  (not-found "<h1>Page not found</h1>"))
+
+(def app (-> main-routes
+             wrap-logging
+             wrap-servlet-session
+             wrap-multipart-params))
+
+(defn run-server []
+  (run-jetty #'app {:port 8080 :join? false}))
