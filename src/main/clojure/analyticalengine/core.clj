5acfec2f188514865f60a9251d3a2eb68c78fb38
Ivan willig <ivan@pluto.(none)>
2011-07-12 15:54:33 -0400

    Added a new dependency and a function to create a basic html page.

--- src/main/clojure/analyticalengine/core.clj
+++ src/main/clojure/analyticalengine/core.clj
@@ -1,9 +1,11 @@
 (ns analyticalengine.core
   (:import [org.geoscript.geocss Translator CssParser]
            [java.io ByteArrayInputStream])
-
+  (:require [hiccup.core :as hiccup]
+            [hiccup.page-helpers :as ph]
+            [hiccup.form-helpers :as fh])
   (:use [compojure.core :only (defroutes GET ANY)]
-        [compojure.route :only (not-found)]
+        [compojure.route :only (not-found files)]
         [geoscript io render]
         [ring.middleware.multipart-params :only (wrap-multipart-params)]
         [ring.adapter.jetty-servlet :only (run-jetty)]))
@@ -20,13 +22,34 @@
             (.get (CssParser/parse
                    (ByteArrayInputStream. (.getBytes string))))))
 
+(defn page [body & {:keys [header title]}]
+  (hiccup/html
+   (ph/doctype :html4)
+   [:head
+    [:title (or title "")]]
+   (ph/include-css "/public/css/screen.css")
+   (ph/include-js "/public/js/jquery-1.5.1.min.js")
+   header
+   [:body
+    [:div {:id :container}
+     [:div {:id "content" }
+      body]]]))
+
 
 (defn index [request]
-  (str request))
+  (page
+   [:div [:h1 "Home page"]
+    [:ul 
+     [:li [:a {:href "/make-buffer"} "Buffer CSV file"]]
+     [:li [:a {:href "/make-distance"} "Distance"]]]]))
 
+(defn make-buffer [request]
+  (page [:div [:h2 "Buffer CSV file"]]))
 
 (defroutes main-routes
   (GET "/" [] index)
+  (GET "/make-buffer" []  make-buffer)
+  (files "/public")
   (not-found "<h1>Page not found</h1>"))
 
 (def app (-> main-routes
