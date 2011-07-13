851c6bc7f8944431d886378a85ec250e2ebd81d9
Ivan willig <ivan@pluto.(none)>
2011-07-13 17:34:19 -0400

    moved more stuff out of core.

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -3,8 +3,8 @@ function loadPage(options){
   var b = new OpenLayers.Bounds(options.bounds[0], options.bounds[1], options.bounds[2], options.bounds[3]);
 
   map = new OpenLayers.Map("maps", {
-    projection: new OpenLayers.Projection("EPSG:4326"),
-    displayProjection: new OpenLayers.Projection("EPSG:4326"),    
+    projection: new OpenLayers.Projection(options.epsg),
+    displayProjection: new OpenLayers.Projection("EPSG:4326"),
     allOverlays: true,
     units: "m",
     numZoomLevels: 22,
@@ -16,7 +16,5 @@ function loadPage(options){
   map.addControl(new OpenLayers.Control.LayerSwitcher());
   map.addControl(new OpenLayers.Control.PanZoom());
   map.zoomToExtent(b);
-
-
 
 }; 
--- src/main/clojure/analyticalengine/core.clj
+++ src/main/clojure/analyticalengine/core.clj
@@ -1,19 +1,10 @@
 (ns analyticalengine.core
-  (:import [org.geoscript.geocss Translator CssParser]
-           [org.apache.commons.configuration HierarchicalINIConfiguration]
-           [org.geotools.geometry.jts ReferencedEnvelope]
-           [java.io File
-            ByteArrayOutputStream
-            ByteArrayInputStream
-            FileInputStream])
-  (:require [hiccup.core :as hiccup]
-            [hiccup.page-helpers :as ph]
-            [hiccup.form-helpers :as fh])
   (:use [compojure.core :only (defroutes GET ANY)]
         [compojure.route :only (not-found files)]
+        [analyticalengine.pages :as pages]
+        [analyticalengine.wms   :as wms]
         [clojure.contrib.json :only (json-str)]
-        [geoscript io render proj]
-        [clojure.contrib.sql :as sql]
+        [ring.middleware.params :only (wrap-params)]
         [ring.middleware.multipart-params :only (wrap-multipart-params)]
         [ring.adapter.jetty-servlet :only (run-jetty)]))
 
@@ -24,112 +15,17 @@
        (assoc request :session (.getSession servlet-request true))
        request))))
 
-(defn make-css-style [string]
-  (.css2sld (Translator.)
-            (.get (CssParser/parse
-                   (ByteArrayInputStream. (.getBytes string))))))
-
-(defn page [body & {:keys [header title]}]
-  (hiccup/html
-   (ph/doctype :html4)
-   [:head
-    [:title (or title "")]]
-   (ph/include-css "/public/css/screen.css")
-   (ph/include-js "/public/js/jquery.min.js")
-   header
-   [:body
-    [:div {:id :container}
-     [:div {:id "content" }
-      body]]]))
-
-(defn make-envelope [request]
-  "Make a Refercened Envelope from a request parameter"
-  (let [[minx miny maxx maxy] (map #(Double/parseDouble %)
-                                  (.split (get (:query-params request) "BBOX"
-                                               ) ","))
-        srs   (epsg->proj (get (:query-params request) "SRS" "EPSG:4326"))]
-    (ReferencedEnvelope. minx maxx miny maxy srs)))
-
-(defn get-datastore [request]
-  (:datastore @(.state (:servlet request))))
-
-(defn get-layer [request layer]
-  (println layer)
-  (.getFeatureSource (get-datastore request) layer))
-
-(defn wms-handler [request]
-  (let [params          (:query-params request)
-        layer           (get-layer request (get params "LAYERS"))
-        output          (ByteArrayOutputStream.)
-        exceptions      (:EXCEPTIONS params)
-        width           (Integer/parseInt (get params "WIDTH" "100"))
-        height          (Integer/parseInt (get params "HEIGHT" "100"))
-        format          (get params "FORMAT" "image/png")
-        service         (get params "SERVICE" "WMS")
-        version         (get params "VERSION" "1.1.0")
-        request-type    (get params "REQUEST" "GetMap")
-        extent          (make-envelope request)
-        ]
-    (render layer
-            output
-            extent
-            :style nil
-            :height height
-            :width width)
-     (ByteArrayInputStream. (.toByteArray output))))
-
-
-(defn index [request]
-  (let [datastore (get-datastore request)]
-    (page 
-     [:div
-      [:ul
-       (map (fn [name]
-              [:li [:a {:href (str "/viewer/" name)} name]])
-            (.getTypeNames datastore))]])))
-
-(defn bbox->vec [bbox]
-  [(.getMinX bbox)
-   (.getMinY bbox)
-   (.getMaxX bbox)
-   (.getMaxY bbox)])
-
-(defn web-viewer [request]
-  (let [layer (get-layer request (:layer (:params request)))]
-    (page
-     [:div [:h3 "Layer viewer"] layer [:div {:id "maps"}]]
-     :header (list
-              (ph/include-js "/public/js/openlayers/OpenLayers.js")
-              (ph/include-js "/public/js/viewer.js")
-              (ph/javascript-tag (str "$(function(){
-                    loadPage({
-                      name:\""(.. layer (getName) (getLocalPart))"\",
-                      bounds:" (json-str (bbox->vec (.getBounds layer)))",
-                    });
-             })"))))))
-
 (defroutes main-routes
-  (GET "/" [] index)
-  (GET "/viewer/:layer" [layer] web-viewer)
-  (GET "/wms" [] wms-handler)
+  (GET "/" [] pages/index)
+  (GET "/viewer/:layer" [layer] pages/web-viewer)
+  (GET "/wms" [] wms/wms-handler)
   (files "/public")
   (not-found "<h1>Page not found</h1>"))
-
-(defmacro with-datastore [store & body]
-  `(try
-    ~@body
-    (finally (.dispose ~store))))
-
-(defn wrap-db-connection [handler]
-  (fn [request]
-    (if (-> request :uri (.startsWith "/public/"))
-      (handler request)
-      (with-datastore (:datastore @(.state (:servlet request)))
-        (handler request)))))
 
 
 
 (def app (-> main-routes
+             wrap-params
              wrap-servlet-session
              wrap-multipart-params))
 
