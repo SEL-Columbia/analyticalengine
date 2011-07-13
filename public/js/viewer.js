71b898e8fca570935c37da65ece0009e80b2552c
Ivan willig <ivan@pluto.(none)>
2011-07-13 15:00:54 -0400

    Added a view javascript file.

---
+++ public/js/viewer.js
@@ -0,0 +1,22 @@
+
+function loadPage(options){ 
+  var b = new OpenLayers.Bounds(options.bounds[0], options.bounds[1], options.bounds[2], options.bounds[3]);
+
+  map = new OpenLayers.Map("maps", {
+    projection: new OpenLayers.Projection("EPSG:4326"),
+    displayProjection: new OpenLayers.Projection("EPSG:4326"),    
+    allOverlays: true,
+    units: "m",
+    numZoomLevels: 22,
+    maxExtent: b,
+  });
+
+  map.addLayer(new OpenLayers.Layer.WMS(options.name, "/wms", {layers: options.name}));
+  map.addControl(new OpenLayers.Control.Navigation());
+  map.addControl(new OpenLayers.Control.LayerSwitcher());
+  map.addControl(new OpenLayers.Control.PanZoom());
+  map.zoomToExtent(b);
+
+
+
+}; 
